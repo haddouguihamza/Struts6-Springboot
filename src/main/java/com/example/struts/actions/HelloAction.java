03ae2c40c09ed79d256fa077209d0c3c8c05116b
hamzaSftwdev <haddouguihamzaengineer@gmail.com>
2025-01-03 16:48:31 +0100

    push first version of application as release candidate (rc) version with major changes following software versioning

--- src/main/java/com/example/struts/actions/HelloAction.java
+++ src/main/java/com/example/struts/actions/HelloAction.java
@@ -1,26 +1,27 @@
 package com.example.struts.actions;
 
+import com.opensymphony.xwork2.ActionSupport;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
-import org.springframework.stereotype.Controller;
 import org.apache.struts2.convention.annotation.Result;
+import org.springframework.stereotype.Controller;
 
 @Controller
 @Namespace("/")
-public class HelloAction {
-	private String message;
+public class HelloAction extends ActionSupport {
+    private String message;
 
-	@Action(value = "hello", results = { @Result(name = "success", location = "/WEB-INF/views/hello.jsp") })
-	public String execute() {
-		message = "Hello from Struts Action!";
-		return "success";
-	}
+    @Action(value = "hello", results = { @Result(name = "success", location = "/views/hello.jsp") })
+    public String get() {
+        message = "Hello from Struts Action!";
+        return SUCCESS;
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public String getMessage() {
+        return message;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
